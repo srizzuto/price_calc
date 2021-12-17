c52b8775c169fc4a1211988eb775fde6d3c88ebe
srizzuto <srizzuto@gmail.com>
2021-12-17 15:14:12 -0300

    set prices table and costumer input

--- src/main.rb
+++ src/main.rb
@@ -1,4 +1,18 @@
 class ItemsList
+
+  def self.show_price_table
+    puts "This week prices table:\n\n"
+    puts "Item    Unit Price      Sale Price"
+    puts "-----------------------------------"
+
+    @store.each { |product|
+      puts "#{product.name}\t$#{product.price}\t\t".concat(product.has_discount? ? "#{product.discount_quantity} for $#{product.discount_price}" : "")
+    }
+
+    puts "\nPlease enter all the items purchased separated by a comma:"
+    @purchased = gets.chomp.split(",")
+  end
+  
   # Program begins...
   # Create products
   @milk = Product.new("Milk", 3.97, 2, 5.00)
